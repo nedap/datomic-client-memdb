2cb61dff757bd38748d15bc3323fbceb124ae6a1
Lennart Buit <lennart.buit@nedap.com>
2019-11-29 11:54:48 +0100

    Actually invoke sync on the right value

--- src/compute/datomic_client_memdb/core.clj
+++ src/compute/datomic_client_memdb/core.clj
@@ -80,14 +80,14 @@
     (-> @(peer/transact conn (:tx-data arg-map))
         (update-vals #{:db-before :db-after} #(LocalDb. % db-name))))
 
+  (sync [this t]
+    (peer/sync conn t))
+
   (tx-range [_ arg-map]
     (peer/tx-range (peer/log conn) (:start arg-map) (:end arg-map)))
 
   (with-db [this]
     (client/db this))
-
-  (sync [this t]
-    (peer/sync this t))
 
   clojure.lang.ILookup
   (valAt [this k]
